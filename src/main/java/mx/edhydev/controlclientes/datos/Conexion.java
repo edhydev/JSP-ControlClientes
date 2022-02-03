aadef44c103f5c7f5ef40c9b607f2115de80a6fa
edhydev <edgar.jimenez.dev@gmail.com>
2022-02-03 09:49:35 -0600

    Se agrega la conexión a la bade de datos

---
+++ src/main/java/mx/edhydev/controlclientes/datos/Conexion.java
@@ -0,0 +1,52 @@
+package mx.edhydev.controlclientes.datos;
+
+import org.apache.commons.dbcp2.BasicDataSource;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class Conexion {
+    private static final String JDBC_URL = "jdbc:mysql://localhost:3306/control_clientes?useSSL=false&useTimezone=true&serverTimezone=UTC&allowPublicKeyRetrieval=true";
+    private static final String JDBC_USER = "edgar";
+    private static final String JDBC_PASSWORD = "1234";
+
+    public static DataSource getDataSource() {
+        BasicDataSource dataSource = new BasicDataSource();
+        dataSource.setUrl(JDBC_URL);
+        dataSource.setUsername(JDBC_USER);
+        dataSource.setPassword(JDBC_PASSWORD);
+        dataSource.setInitialSize(12);
+        return dataSource;
+    }
+
+    public static Connection getConnection() throws SQLException {
+        return getDataSource().getConnection();
+    }
+
+    public static void close(ResultSet resultSet) {
+        try {
+            resultSet.close();
+        } catch (SQLException e) {
+            e.printStackTrace(System.out);
+        }
+    }
+
+    public static void close(PreparedStatement statement) {
+        try {
+            statement.close();
+        } catch (SQLException e) {
+            e.printStackTrace(System.out);
+        }
+    }
+
+    public static void close(Connection connection) {
+        try {
+            connection.close();
+        } catch (SQLException e) {
+            e.printStackTrace(System.out);
+        }
+    }
+}
