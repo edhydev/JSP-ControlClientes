0a2a1e7450ef8b21f3288e8eb6458896f3bdb464
edhydev <edgar.jimenez.dev@gmail.com>
2022-02-03 15:26:35 -0600

    Cambio de Contexto

---
+++ src/main/java/mx/edhydev/controlclientes/web/ServletControlador.java
@@ -0,0 +1,25 @@
+package mx.edhydev.controlclientes.web;
+
+import mx.edhydev.controlclientes.datos.ClienteDao;
+import mx.edhydev.controlclientes.datos.impl.ClienteDaoImpl;
+import mx.edhydev.controlclientes.dominio.Cliente;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+
+@WebServlet("/ServletControlador")
+public class ServletControlador extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        ClienteDao clienteDao = new ClienteDaoImpl();
+        List<Cliente> clientes = clienteDao.listar();
+        System.out.println("clientes: " + clientes);
+        request.setAttribute("clientes", clientes);
+        request.getRequestDispatcher("clientes.jsp").forward(request, response);
+    }
+}
